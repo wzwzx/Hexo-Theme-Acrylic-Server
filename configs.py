ff5588a461779b58d3e0309c267daa2b267e89cb
花 <antidooo@163.com>
2023-03-18 07:39:50 +0800

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,13 +1,13 @@
 import pymysql
 pymysql.install_as_MySQLdb()
-DOMAINS = ["127.0.0.1", "yoursite.com"]
+DOMAINS = ["127.0.0.1", "acr.wzwzx.cn"]
 DATABASES = {
     'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'qexo',
-            'USER': 'root',
-            'PASSWORD': '123456',
-            'HOST': '127.0.0.1',
+            'NAME': 'wzwzx',
+            'USER': '7vtw1hi02bxtjyuyvmjb',
+            'PASSWORD': 'pscale_pw_343gf40E6VYQI3hRTchkYxxHIDDZ4XFeXqW1DRwjAfz',
+            'HOST': 'us-east.connect.psdb.cloud',
             'PORT': '3306',
             'OPTIONS': {
                 "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
